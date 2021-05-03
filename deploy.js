b20af935f990bdc80de98f6537878d49fc07a004
Darren <Tsukasa589@gmail.com>
2021-05-03 03:08:53 -0400

    PEM file
    Added .pem file to replace .ppk file. Changes to deploy.js have been made accordingly.

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '54.175.145.120',
       username: 'ubuntu',
-      privateKey: 'riot-pass.ppk'
+      privateKey: 'riot-pass.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
